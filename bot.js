b9a0f89d7d2e291dd1deb9cd8a98c88743c2b85d
varlock3 <44277067+varlock3@users.noreply.github.com>
2018-11-16 22:42:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,8 @@ A7MD.on("guildMemberAdd", member => {
 **
 سلام عليكم 
 لو سمحت ممكن تدخل سيرفرى
-                               [ https://discord.gg/cNj9QvN] 
+                               [ https://discord.gg/whh6Y73
+https://discord.gg/cNj9QvN] 
 الدعووة خاصة لك ... [ ${member}  ]
 **`) 
 }).catch(console.error)
